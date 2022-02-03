c823c3202611691501430bfc01be27d28a378c69
Joe Schlimmer <joe@noreply.localhost>
2022-02-03 10:59:05 -0500

    Add 't2-chip.sh'

---
+++ t2-chip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CPU="$(/usr/sbin/system_profiler SPHardwareDataType | grep 'Apple M1')"
+IBRIDGE="$(/usr/sbin/system_profiler SPiBridgeDataType | /usr/bin/grep 'Model Name')"
+
+if [[ -z "$CPU" ]]; then
+	if [[ "${IBRIDGE}" == "" ]]; then
+    	/bin/echo "<result>None</result>"
+	else
+    	/bin/echo "<result>${IBRIDGE}</result>"
+    fi
+else
+	/bin/echo "<result>${CPU} (T2 Integrated)</result>"
+fi
+
+exit 0
