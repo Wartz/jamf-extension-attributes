cd0654161c3eace7fcdaaa2645e1f1299f58f668
Joe Schlimmer <joe@noreply.localhost>
2022-02-03 11:05:17 -0500

    Update 't2-chip.sh'

--- t2-chip.sh
+++ t2-chip.sh
@@ -4,13 +4,13 @@ CPU="$(/usr/sbin/system_profiler SPHardwareDataType | grep 'Apple M1')"
 IBRIDGE="$(/usr/sbin/system_profiler SPiBridgeDataType | /usr/bin/grep 'Model Name')"
 
 if [[ -z "$CPU" ]]; then
-	if [[ "${IBRIDGE}" == "" ]]; then
+    if [[ "${IBRIDGE}" == "" ]]; then
         /bin/echo "<result>None</result>"
-	else
+    else
         /bin/echo "<result>${IBRIDGE}</result>"
     fi
 else
-	/bin/echo "<result>${CPU} (T2 Integrated)</result>"
+    /bin/echo "<result>${CPU} (T2 Integrated)</result>"
 fi
 
 exit 0
